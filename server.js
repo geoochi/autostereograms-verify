c2436ac3dbd7f556ceea7a392ae7493c915520f0
Geoo Chi <geoooochi@gmail.com>
2025-11-01 17:30:25 +0800

    update

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
 
 const app = express()
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3001
 
 // 解析 JSON 请求体
 app.use(express.json())
